d8c824503fb6a5bfd41968630be530d485918223
Rico Jones <rico@toasterlovin.com>
2016-02-05 10:12:56 -0800

    Install libfontconfig dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,7 @@ apt-get install -y tmux
 apt-get install -y silversearcher-ag
 
 # PhantonJS
+apt-get install -y libfontconfig
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 rm phantomjs-2.1.1-linux-x86_64.tar.bz2
